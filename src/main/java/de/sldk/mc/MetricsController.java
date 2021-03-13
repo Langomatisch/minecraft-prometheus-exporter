9fb5c38f0b369c77bd061abcebe46c949f4665e5
Matthew <admin@langomatisch.de>
2021-03-13 13:04:02 +0100

    made compatible with java version 8

--- src/main/java/de/sldk/mc/MetricsController.java
+++ src/main/java/de/sldk/mc/MetricsController.java
@@ -2,12 +2,12 @@ package de.sldk.mc;
 
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.common.TextFormat;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
